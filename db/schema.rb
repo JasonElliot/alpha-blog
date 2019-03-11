78d61948b6ed63087e5b45625e997b3361ce7b8d
subu <subunew@gmail.com>
2019-03-11 17:20:44 +0530

    some thing done again

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_063920) do
+ActiveRecord::Schema.define(version: 2019_03_11_114023) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
+  create_table "article_ctegories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
